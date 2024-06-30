59ae6ffed5a928e23db70434c6b89d80bcf6af91
Sharan Thakur <sharandt19@live.com>
2024-06-30 12:06:49 +0530

    Initial Commit

---
+++ TTSAudioPlayer/TTSAudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTSAudioPlayerApp.swift
+//  TTSAudioPlayer
+//
+//  Created by Sharan Thakur on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TTSAudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
